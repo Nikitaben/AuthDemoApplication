d47fbf0d4d4bd50c6e89def11b3d37c182a0e244
Nikitaben <nikitaprajapati88@gmail.com>
2021-09-24 12:23:44 -0600

    add auth jwt

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
 
+
+const mysql = require('mysql2')
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Nikita',
+  database: 'auth_db_demo'
+});
+const SK = "secretKey";
+
+router.get("/from_db", (req, res) => {
+   connection.connect();
+
+   connection.query("select * from user", function (err, rows, fields) {
+    if (err) throw err
+    res.send(rows);
+   });
+   connection.end();
+});
 const users = [
     { id: 1, email: "aaa@aaa.com", password: "1234"},
     { id: 2, email: "bbb@bbb.com", password: "1111"},
@@ -18,7 +38,7 @@ router.get("/products", (req, res) => {
    //console.log("Hello from API"); 
    res.status(200).send(list);
 });
-router.get("/premium", (req, res) => {
+router.get("/premium", verifyToken, (req, res) => {
 
     const list = [
         {id: 1, name: "Tandoor Desi Pizza premium", price: 34.99 },
@@ -29,7 +49,9 @@ router.get("/premium", (req, res) => {
    res.status(200).send(list);
 });
 
-
+router.get("/users", (req, res) => {
+    res.send(users);
+  });
 router.get('/:id', (req, res) => {
     const user = users.find((u) => u.id == +req.params.id)
     res.send(user);
@@ -37,6 +59,49 @@ router.get('/:id', (req, res) => {
 router.post("/register", (req, res) => {
     let userData = req.body;
 
-    res.send(userData);
+    const id = Math.max.apply(Math, users.map((u ) => u.id)) + 1;
+    userData["id"] = id;
+    users.push(userData);
+       
+    const payload = { subject: id };
+   const token = jwt.sign(payload, SK);
+    //console.log(token);
+    res.send({token});
 });
+router.post('/login', (req, res) => {
+    const userData = req.body;
+    const user = users.find(u => u.email === userData["email"]);
+
+    if (!user) {
+        res.status(401).send("Incorrect email")
+    }
+    else if (user.password !== userData["password"]) {
+        res.status(401).send("Incorrect password");
+      } else {
+       const payload = { subject: user.id };
+       const token = jwt.sign(payload, SK);
+        res.status(200).send( {token} );
+      }
+});
+function verifyToken( req, res, next){
+  if( !req.headers.authorization) {
+     return res.status(401).send("Unauthorized request");
+  }
+
+  const token = req.headers.authorization.split(' ')[1];
+  if(token === 'null') {
+    return res.status(401).send("Unauthorized request");
+  }
+  try {
+    const check = jwt.verify(token, SK);
+
+    if (!check) {
+      return res.status(401).send("Unauthorized request");
+    }
+
+    next();
+  } catch (err) {
+    return res.status(418).send("Somethig wrong with token");
+  }
+}
 module.exports = router;
